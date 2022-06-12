4fec6f9b2572eca8d78e93e22a497b01bc1767ac
JMSEhsan@gmail.com <Ehsan Jalali>
2022-06-11 23:56:17 -0600

    commit

--- ArcLim.py
+++ ArcLim.py
@@ -1,8 +1,8 @@
 from sympy import *
 x = symbols('x')
-f = 2*(pi/2-(atan(x)-atan(1/x)))*x/cos(atan(x)-atan(1/x))#2*(atan (1/(1+x))/sin (atan (1/(1+x)))*(1+x)) - 2*x - pi
-g = 2*(atan (1/(1+x))/sin(atan (1/(1+x)))*(1+x))
-resultf = limit(f, x, 10)
+f = 2*(pi/2-(atan(x)-atan(1/x)))*x/cos(atan(x)-atan(1/x)) - 2*(x-1) - pi
+g = x*sin(x)/(1-cos(x))
+resultf = limit(f, x, 2*pi)
 resultg = limit(g, x, 0)
 print("Limit of the difference between arc of a half circle with radius of 1 and an arc, which touches the half circle on top, with a chord tending to infinity: ", resultf) 
-print("limit 2 : ", resultg)
+print("limit g(x) = ", resultg)
