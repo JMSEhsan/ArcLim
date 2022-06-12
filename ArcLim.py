dac480160492eda3eaa10f1f17288ce4fc3022af
JMSEhsan@gmail.com <Ehsan Jalali>
2022-06-12 14:19:57 -0600

    Commit, Finding limits using sympy

--- ArcLim.py
+++ ArcLim.py
@@ -1,8 +1,12 @@
+#Finding limits using sympy
 from sympy import *
 x = symbols('x')
-f = 2*(pi/2-(atan(x)-atan(1/x)))*x/cos(atan(x)-atan(1/x)) - 2*(x-1) - pi
+n = symbols('n')
+f = (2*(pi/2-(atan(x)-atan(1/x)))*x/cos(atan(x)-atan(1/x)) - 2*(x-1) - pi).doit()  # just an idea, needs more work
 g = x*sin(x)/(1-cos(x))
-resultf = limit(f, x, 2*pi)
+s = Sum (((sqrt(2) - 1)/sqrt(2))**n,(n,0,oo)).doit()
+resultf = limit(f, x, 1.1)
 resultg = limit(g, x, 0)
-print("Limit of the difference between arc of a half circle with radius of 1 and an arc, which touches the half circle on top, with a chord tending to infinity: ", resultf) 
-print("limit g(x) = ", resultg)
+print("the lmit of the difference between arc of a half circle with radius of 1 and an arc, which touches the half circle on top, with a chord tending to infinity:    ", resultf) 
+print("Where x --> 0, the limit of g(x) = x*sin(x)/(1-cos(x)) =    ", resultg)
+print("Where n from 0 --> + infinity, the divergence limit of the series: Sum (((sqrt(2) - 1)/sqrt(2))^n =    ", s)
